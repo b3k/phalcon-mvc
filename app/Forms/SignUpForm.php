db4cd63f87fe14c1d37557521e023d3e347d6ead
Bartosz Pietrzak <b3k@b3k.pl>
2014-08-27 21:21:55 +0200

    some changes

--- app/Forms/SignUpForm.php
+++ app/Forms/SignUpForm.php
@@ -5,6 +5,7 @@ namespace App\Forms;
 use App\Library\Form\Base\BaseForm;
 use Phalcon\Validation\Validator\Email;
 use Phalcon\Validation\Validator\StringLength;
+use Phalcon\Validation\Validator\Confirmation;
 
 class SignUpForm extends BaseForm
 {
@@ -17,14 +18,14 @@ class SignUpForm extends BaseForm
                 ->setAttribute('autocomplete', 'off');
 
         $content = $this->addContentFieldSet()
-                ->addText('fullname', null, null, null, [], ['autocomplete' => 'off'])
-                ->addPassword('password', null, null, [], ['autocomplete' => 'off'])
-                ->addPassword('confirm_password', null, null, [], ['autocomplete' => 'off'])
-                ->addText('email', null, null, null, [], ['autocomplete' => 'off']);
+                ->addText('fullname',               'Fullname', null, null, [], ['autocomplete' => 'off', 'placeholder' => 'Your fullname'])
+                ->addText('email',                  'Email',    null, null, [], ['autocomplete' => 'off', 'placeholder' => 'Email'])
+                ->addPassword('password',           'Password', null, [], ['autocomplete' => 'off'])
+                ->addPassword('confirm_password',   'Confirm password', null, [], ['autocomplete' => 'off']);
 
         $this->addFooterFieldSet()
                 ->addButton('create')
-                ->addButtonLink('cancel', 'Cancel', ['for' => 'admin-users']);
+                ->addButtonLink('cancel', 'Cancel', ['for' => 'homepage']);
 
         $this->_setValidation($content);
     }
@@ -32,10 +33,11 @@ class SignUpForm extends BaseForm
     protected function _setValidation($content)
     {
         $content->getValidation()
-                ->add('fullname', new StringLength(['min' => 2]))
-                ->add('email', new Email())
-                ->add('password', new StringLength(['min' => 6]))
-                ->add('confirm_password', new StringLength(['min' => 6]));
+                ->add('fullname',   new StringLength(['min' => 2]))
+                ->add('email',      new Email())
+                ->add('password',   new StringLength(['min' => 6]))
+                ->add('confirm_password', new StringLength(['min' => 6]))
+                ->add('confirm_password', new Confirmation(['message' => 'Password doesn\'t match confirmation', 'with' => 'password']));
 
         $content
                 ->setRequired('fullname')
@@ -44,88 +46,8 @@ class SignUpForm extends BaseForm
                 ->setRequired('confirm_password');
 
         $this
+                ->addFilter('email', self::FILTER_EMAIL)
                 ->addFilter('password', self::FILTER_STRING)
                 ->addFilter('confirm_password', self::FILTER_STRING);
     }
-
-    /* public function initialize($entity = null, $options = null)
-      {
-      $name = new Text('name');
-      $name->setLabel('Name');
-      $name->addValidators(array(
-      new PresenceOf(array(
-      'message' => 'The name is required'
-      ))
-      ));
-      $this->add($name);
-
-      // Email
-      $email = new Text('email');
-      $email->setLabel('E-Mail');
-      $email->addValidators(array(
-      new PresenceOf(array(
-      'message' => 'The e-mail is required'
-      )),
-      new Email(array(
-      'message' => 'The e-mail is not valid'
-      ))
-      ));
-
-      $this->add($email);
-      // Password
-      $password = new Password('password');
-      $password->setLabel('Password');
-      $password->addValidators(array(
-      new PresenceOf(array(
-      'message' => 'The password is required'
-      )),
-      new StringLength(array(
-      'min' => 8,
-      'messageMinimum' => 'Password is too short. Minimum 8 characters'
-      )),
-      new Confirmation(array(
-      'message' => 'Password doesn\'t match confirmation',
-      'with' => 'confirmPassword'
-      ))
-      ));
-
-      $this->add($password);
-
-      // Confirm Password
-      $confirmPassword = new Password('confirmPassword');
-      $confirmPassword->setLabel('Confirm Password');
-      $confirmPassword->addValidators(array(
-      new PresenceOf(array(
-      'message' => 'The confirmation password is required'
-      ))
-      ));
-
-      $this->add($confirmPassword);
-      // Remember
-      $terms = new Check('terms', array(
-      'value' => 'yes'
-      ));
-      $terms->setLabel('Accept terms and conditions');
-
-      $terms->addValidator(new Identical(array(
-      'value' => 'yes',
-      'message' => 'Terms and conditions must be accepted'
-      )));
-      $this->add($terms);
-
-      // CSRF
-      $csrf = new Hidden('csrf');
-
-      $csrf->addValidator(new Identical(array(
-      'value' => $this->security->getSessionToken(),
-      'message' => 'CSRF validation failed'
-      )));
-
-      $this->add($csrf);
-
-      // Sign Up
-      $this->add(new Submit('Sign Up', array(
-      'class' => 'btn btn-success'
-      )));
-      } */
 }
